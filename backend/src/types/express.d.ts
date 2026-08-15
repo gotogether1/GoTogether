efbb57eb717810a63d8a56bc757a1a851550e1cd
ADITHYA <adithya@ADITHYAs-MacBook-Air.local>
2026-08-15 19:59:49 +0530

    Remove all repository contents

--- backend/src/types/express.d.ts
+++
@@ -1,9 +0,0 @@
-declare namespace Express {
-  export interface Request {
-    auth?: {
-      uid: string;
-      email?: string;
-      providerIds?: string[];
-    };
-  }
-}
--- mobile/src/components/loading/index.ts
+++
@@ -1,6 +0,0 @@
-export * from './PageLoader';
-export * from './SkeletonCard';
-export * from './InlineLoader';
-export * from './ButtonLoader';
-export * from './LoadingOverlay';
-export * from './EmptyState';
